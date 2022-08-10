4c47e2f9e9dbb2fcafa2cd2d460f26886f8c8d9f
anshrusia200 <anshrusia2002@gmail.com>
2022-08-10 23:32:12 +0530

    heroku deploy

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,26 +1,26 @@
-const sgMail = require('@sendgrid/mail')
+const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendWelcomeEmail = (email, name) => {
-    sgMail.send({
-        to : email,
-        from : 'rusiaansh123@gmail.com',
-        subject : 'Thanks for joining in!',
-        text : `Welcome to the app, ${name}. Let me know how you get along with the app.`
-    })
-}  
+  sgMail.send({
+    to: email,
+    from: "rusiaansh123@gmail.com",
+    subject: "Thanks for joining in!",
+    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
+  });
+};
 
 const sendCancelEmail = (email, name) => {
-    sgMail.send({
-        to : email,
-        from : 'rusiaansh123@gmail.com',
-        subject : 'Account cancelled !',
-        text : `Good Bye ${name}. Thanks for being a part of our organisation. Please let me know what could have I done to make your experience better and make you stay. Sorry to see you go and hope to see you again.`
-    })
-}    
+  sgMail.send({
+    to: email,
+    from: "rusiaansh123@gmail.com",
+    subject: "Account cancelled !",
+    text: `Good Bye ${name}. Thanks for being a part of our organisation. Please let me know what could have I done to make your experience better and make you stay. Sorry to see you go and hope to see you again.`,
+  });
+};
 
 module.exports = {
-    sendWelcomeEmail, // sendWelcomeEmail : sendWelcomeEmail ko es6 shorthand se likha h 
-    sendCancelEmail
-}
+  sendWelcomeEmail, // sendWelcomeEmail : sendWelcomeEmail ko es6 shorthand se likha h
+  sendCancelEmail,
+};
--- src/index.js
+++ src/index.js
@@ -45,8 +45,12 @@ app.use(flash());
 app.use(userRouter);
 app.use(taskRouter);
 
-app.listen(port, () => {
-  console.log("Server is up on port " + port);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
 
 // const multer = require('multer')
